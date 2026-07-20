eb83d8e45fabe7fee0ee0f736e67e1485205d507
YT-MrXT <canalmrxt@gmail.com>
2026-07-20 19:16:09 +0100

    Add extracted source files

--- Bloxstrap/LaunchHandler.cs
+++ Bloxstrap/LaunchHandler.cs
@@ -115,7 +115,7 @@ namespace Bloxstrap
             else
             {
 #if QA_BUILD
-                Frontend.ShowMessageBox("You are about to install a QA build of Bloxstrap. The red window border indicates that this is a QA build.\n\nQA builds are handled completely separately of your standard installation, like a virtual environment.", MessageBoxImage.Information);
+                Frontend.ShowMessageBox("You are about to install a QA build of Mrstrap. The red window border indicates that this is a QA build.\n\nQA builds are handled completely separately of your standard installation, like a virtual environment.", MessageBoxImage.Information);
 #endif
 
                 new LanguageSelectorDialog().ShowDialog();
